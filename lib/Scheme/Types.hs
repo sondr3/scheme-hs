146e2b77105f4ea7b12b55d502a4a4ba4e1efde4
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-02 23:35:10 +0100

    Start working on operators for numeric tower

--- lib/Scheme/Types.hs
+++ lib/Scheme/Types.hs
@@ -2,7 +2,7 @@ module Scheme.Types where
 
 import Data.Array (Array, elems)
 import qualified Data.ByteString as BS
-import Data.Complex (Complex, imagPart, realPart)
+import Data.Complex (Complex ((:+)), imagPart, realPart)
 import qualified Data.Map.Strict as Map
 import Data.Ratio (denominator, numerator)
 import Data.Text (Text)
@@ -17,6 +17,43 @@ data Number
   | Rational Rational
   | Complex (Complex Double)
   deriving (Show)
+
+instance Num Number where
+  (+) (Integer x) (Integer y) = Integer (x + y)
+  (+) (Integer x) (Real y) = Real (fromInteger x + y)
+  (+) (Real x) (Integer y) = Real (x + fromInteger y)
+  (+) (Integer x) (Rational y) = Rational (fromInteger x + y)
+  (+) (Rational x) (Integer y) = Rational (x + fromInteger y)
+  (+) (Integer x) (Complex y) = Complex (fromInteger x + y)
+  (+) (Complex x) (Integer y) = Complex (x + fromInteger y)
+  (+) (Real x) (Real y) = Real (x + y)
+  (+) (Real x) (Rational y) = Rational (toRational x + y)
+  (+) (Rational x) (Real y) = Rational (x + toRational y)
+  (+) (Real x) (Complex y) = Complex ((x :+ 0) + y)
+  (+) (Complex x) (Real y) = Complex (x + (y :+ 0))
+  (+) (Rational x) (Rational y) = Rational (x + y)
+  (+) (Rational x) (Complex y) = Complex (x + y)
+  (+) (Complex x) (Rational y) = Complex (x + y)
+  (+) (Complex x) (Complex y) = Complex (x + y)
+  (*) (Integer x) (Integer y) = Integer (x * y)
+  (*) (Real x) (Real y) = Real (x + y)
+  (*) (Rational x) (Rational y) = Rational (x + y)
+  (*) (Complex x) (Complex y) = Complex (x + y)
+  (*) _ _ = error "undefined operation"
+  (-) (Integer x) (Integer y) = Integer (x - y)
+  (-) (Real x) (Real y) = Real (x + y)
+  (-) (Rational x) (Rational y) = Rational (x + y)
+  (-) (Complex x) (Complex y) = Complex (x + y)
+  (-) _ _ = error "undefined operation"
+  abs (Integer x) = Integer (abs x)
+  abs (Real x) = Real (abs x)
+  abs (Rational x) = Rational (abs x)
+  abs _ = error "undefined operation"
+  signum (Integer x) = Integer (signum x)
+  signum (Real x) = Real (signum x)
+  signum (Rational x) = Rational (signum x)
+  signum _ = error "undefined operation"
+  fromInteger x = Integer x
 
 showNumber :: Number -> String
 showNumber (Integer i) = show i
