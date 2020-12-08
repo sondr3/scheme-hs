63ec2a05380d263482f4a8fd8619cb78bc742656
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-09 00:26:45 +0100

    Rework environment to use [(Text, Val)] and IO

--- lib/Scheme/Environment.hs
+++ lib/Scheme/Environment.hs
@@ -1,19 +1,22 @@
 module Scheme.Environment where
 
-import qualified Data.Map.Strict as Map
+import Control.Exception (throw)
+import Control.Monad.Except (MonadIO (liftIO), throwError)
+import Data.IORef (newIORef, readIORef, writeIORef)
 import Data.Text (Text)
 import Scheme.Primitives (numericPrimitives)
-import Scheme.Types (Env, SchemeM, SchemeVal (..), showVal)
+import Scheme.Types (Env, Fn (..), IOSchemeResult, SchemeError (..), SchemeResult, SchemeVal (..), showVal)
 
-primitives :: [(Text, [SchemeVal] -> SchemeM SchemeVal)]
+primitives :: [(Text, [SchemeVal] -> SchemeResult SchemeVal)]
 primitives = numericPrimitives
 
-buildEnvironment :: Map.Map Text SchemeVal
-buildEnvironment = Map.fromList (map createPrimFun primitives)
+nullEnv :: IO Env
+nullEnv = newIORef []
 
--- createPrimFun :: (Text, [SchemeVal] -> Either SchemeError SchemeVal) -> (Text, SchemeVal)
--- createPrimFun :: (a, [SchemeVal] -> Either SchemeError SchemeVal) -> (a, SchemeVal)
-createPrimFun :: (a, [SchemeVal] -> SchemeM SchemeVal) -> (a, SchemeVal)
+buildEnvironment :: IO Env
+buildEnvironment = nullEnv >>= flip bindVariables (map createPrimFun primitives)
+
+createPrimFun :: (a, [SchemeVal] -> SchemeResult SchemeVal) -> (a, SchemeVal)
 createPrimFun (sym, func) = (sym, Primitive func)
 
 createFun ::
@@ -28,8 +31,8 @@ createFun ::
   -- | Environment closure
   Env ->
   -- | Resulting function
-  SchemeVal
-createFun macro varargs params = Fun macro (map showVal params) varargs
+  IOSchemeResult SchemeVal
+createFun macro varargs params body closure = return $ Fun $ Fn macro (map showVal params) varargs body closure
 
 createNormalFun ::
   -- | Parameters
@@ -37,14 +40,53 @@ createNormalFun ::
   -- | Body
   [SchemeVal] ->
   Env ->
-  SchemeVal
+  IOSchemeResult SchemeVal
 createNormalFun = createFun False Nothing
 
-createVariadicFun :: SchemeVal -> [SchemeVal] -> [SchemeVal] -> Env -> SchemeVal
+createVariadicFun :: SchemeVal -> [SchemeVal] -> [SchemeVal] -> Env -> IOSchemeResult SchemeVal
 createVariadicFun = createFun False . Just . showVal
 
-createMacro :: [SchemeVal] -> [SchemeVal] -> Env -> SchemeVal
+createMacro :: [SchemeVal] -> [SchemeVal] -> Env -> IOSchemeResult SchemeVal
 createMacro = createFun True Nothing
 
-bindVariables :: Env -> [(Text, SchemeVal)] -> Env
-bindVariables old closure = Map.union old (Map.fromList closure)
+isBound :: Env -> Text -> IO Bool
+isBound envRef var = do
+  env <- readIORef envRef
+  case lookup var env of
+    Just _ -> return True
+    Nothing -> return False
+
+withVar :: Env -> Text -> IOSchemeResult SchemeVal
+withVar envRef var = do
+  env <- liftIO $ readIORef envRef
+  case lookup var env of
+    Just val -> liftIO $ readIORef val
+    Nothing -> throw $ UnboundSymbol var
+
+getVariable :: Text -> Env -> IOSchemeResult SchemeVal
+getVariable var envRef = withVar envRef var
+
+setVariable :: Env -> Text -> SchemeVal -> IOSchemeResult SchemeVal
+setVariable envRef var val = do
+  env <- liftIO $ readIORef envRef
+  maybe (throwError $ UnboundSymbol var) (liftIO . flip writeIORef val) (lookup var env)
+  return val
+
+defineVariable :: Env -> Text -> SchemeVal -> IOSchemeResult SchemeVal
+defineVariable envRef var val = do
+  isDefined <- liftIO $ isBound envRef var
+  if isDefined
+    then setVariable envRef var val >> return val
+    else liftIO $ do
+      valRef <- newIORef val
+      env <- readIORef envRef
+      writeIORef envRef ((var, valRef) : env)
+      return val
+
+bindVariables :: Env -> [(Text, SchemeVal)] -> IO Env
+bindVariables envRef closure = readIORef envRef >>= extendEnv closure >>= newIORef
+  where
+    extendEnv cls env = fmap (++ env) (mapM addBinding cls)
+    addBinding (var, val) = do
+      valRef <- newIORef val
+      return (var, valRef)
