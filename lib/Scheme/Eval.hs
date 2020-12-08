5de081ed59ff4431aa25c2ed1ea85467ee11256f
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-08 16:32:57 +0100

    And finally update and fix evaluation

--- lib/Scheme/Eval.hs
+++ lib/Scheme/Eval.hs
@@ -2,63 +2,34 @@
 
 module Scheme.Eval where
 
-import Control.Exception (SomeException, fromException, throw, try)
-import Control.Monad.Except (MonadError (throwError))
-import Control.Monad.Reader (ask, asks, local, runReaderT)
+import Control.Exception (throw)
+import Control.Monad.Except (MonadError, runExceptT, throwError)
+import Control.Monad.Reader (ask, local, runReaderT)
 import qualified Data.Map.Strict as Map
 import Data.Text (Text)
-import qualified Data.Text as T
-import Scheme.Environment (buildEnvironment, extractVariable, getVariable)
-import Scheme.Parser (parseFile, parseInput)
-import Scheme.Types (Environment, Eval, Function (..), SchemeError (..), SchemeVal (..), showError, showVal, unEval)
-
-safeExex :: IO a -> IO (Either String a)
-safeExex m = do
-  res <- try m
-  case res of
-    Left (err :: SomeException) ->
-      case fromException err of
-        Just (enclosed :: SchemeError) -> return $ Left (show enclosed)
-        Nothing -> return $ Left (show err)
-    Right val -> return $ Right val
-
-evalFile :: FilePath -> Text -> IO ()
-evalFile file content = runInEnv buildEnvironment (fileToEvalForm file content) >>= print
+import Scheme.Environment (buildEnvironment)
+import Scheme.Parser (parseInput)
+import Scheme.Types (Env, SchemeError (..), SchemeM, SchemeVal (..), showError, showVal, unScheme)
 
 evalLine :: Text -> IO ()
-evalLine content = runInEnv buildEnvironment (lineToEvalForm content) >>= print
+evalLine input = runWithEnv buildEnvironment evalForm >>= print
+  where
+    evalForm = either (throw . ParserError . showError) eval $ parseInput input
 
-lineToEvalForm :: Text -> Eval SchemeVal
-lineToEvalForm content = either (throw . ParserError . showError) eval $ parseInput content
+runWithEnv :: Env -> SchemeM a -> IO (Either SchemeError a)
+runWithEnv env expr = runExceptT (runReaderT (unScheme expr) env)
 
-fileToEvalForm :: FilePath -> Text -> Eval SchemeVal
-fileToEvalForm file content = either (throw . ParserError . showError) eval $ parseFile file content
-
-showParse :: Text -> Text
-showParse input = either showError showVal $ parseInput input
-
-runInEnv :: Environment -> Eval b -> IO b
-runInEnv code action = runReaderT (unEval action) code
-
-ensureSymbol :: SchemeVal -> Eval SchemeVal
-ensureSymbol n@(Symbol _) = pure n
-ensureSymbol n = throw $ TypeMismatch "symbol" n
-
-bindArgsEval :: [SchemeVal] -> [SchemeVal] -> SchemeVal -> Eval SchemeVal
-bindArgsEval params args expr = do
-  env <- ask
-  local (const (Map.fromList (zipWith (\a b -> (extractVariable a, b)) params args) <> env)) $ eval expr
-
-eval :: SchemeVal -> Eval SchemeVal
+eval :: SchemeVal -> SchemeM SchemeVal
 eval Nil = return Nil
 eval (List []) = return Nil
 eval val@(Character _) = return val
-eval val@(Integer _) = return val
-eval val@(Real _) = return val
-eval val@(Rational _) = return val
-eval val@(Complex _) = return val
+eval val@(Number _) = return val
 eval val@(Boolean _) = return val
-eval val@(Symbol _) = getVariable val
+eval (Symbol sym) = do
+  env <- ask
+  case Map.lookup sym env of
+    Just x -> return x
+    Nothing -> throw $ UnboundSymbol sym
 eval (List [Symbol "quote", xs]) = return xs
 eval (List [Symbol "if", test, cons, alt]) = do
   eval test >>= \case
@@ -78,23 +49,23 @@ eval (List (Symbol "define" : List (Symbol variable : formals) : body)) = undefi
 -- (define (〈variable〉.〈formal〉)〈body〉)
 eval (List (Symbol "define" : PairList (Symbol variable : formals) vararg : body)) = undefined
 -- Lambda function of the form (lambda (x y) (+ x y))
-eval (List [Symbol "lambda", List formals, body]) = asks (Lambda (Function $ applyLambda body formals))
+eval (List [Symbol "lambda", List formals, body]) = undefined
 -- Lambda function of the form (lambda (x y . z) z)
 eval (List (Symbol "lambda" : PairList formals vararg : body)) = undefined
 -- asks (Lambda (Function $ applyLambda [] body))
 -- Lambda function of the form (lambda x x)
-eval (List [Symbol "lambda", formal@(Symbol _), body]) = asks (Lambda (Function $ applyLambda body []))
+eval (List [Symbol "lambda", formal@(Symbol _), body]) = undefined
 -- Application of functions :D
 eval (List ((:) fun exprs)) = do
   func <- eval fun
   val <- mapM eval exprs
   case func of
-    (Fun (Function fn)) -> fn val
-    (Lambda (Function fn) closure) -> local (const closure) $ fn val
+    (Primitive fn) -> fn val
+    -- (Fun {}) -> local (const closure) $ fn val
     _ -> throw $ NotFunction func
 eval xs = throw (Generic $ "Unknown: " <> showVal xs)
 
-evalBody :: SchemeVal -> Eval SchemeVal
+evalBody :: SchemeVal -> SchemeM SchemeVal
 evalBody (List [List ((:) (Symbol "define") [Symbol var, defExpr]), rest]) = do
   val <- eval defExpr
   env <- ask
@@ -105,9 +76,6 @@ evalBody (List ((:) (List ((:) (Symbol "define") [Symbol var, defExpr])) rest)) 
   let envFn = const $ Map.insert var val env
    in local envFn $ evalBody $ List rest
 evalBody body = eval body
-
-applyLambda :: SchemeVal -> [SchemeVal] -> [SchemeVal] -> Eval SchemeVal
-applyLambda expr params args = bindArgsEval params args expr
 
 liftThrows :: MonadError e m => Either e a -> m a
 liftThrows (Right val) = return val
