79a9ba9a253aa3b2f65018e8f30328214fe8a2e9
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-12-10 13:57:27 +0100

    Use Haskeline for REPL instead of selfwritten garbage

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
-import Scheme (repl)
+import Control.Monad.Cont (MonadTrans (lift))
+import Data.Functor ((<&>))
+import qualified Data.Text as T
+import Scheme
+import System.Console.Haskeline
 import System.Environment (getArgs)
 
 help :: IO ()
@@ -12,11 +16,45 @@ help =
     \  scheme -e <expr>\n\
     \  scheme\n"
 
+evalStringAndShow :: Env -> String -> IO String
+evalStringAndShow env expr =
+  liftIOThrows $
+    (liftThrows . readManyExpr) (T.pack expr)
+      >>= fmap nilOrLast . mapM (eval env)
+      <&> showVal
+      <&> T.unpack
+  where
+    nilOrLast x = if null x then List [] else last x
+
+evalAndPrint :: Env -> String -> InputT IO ()
+evalAndPrint env expr = do
+  out <- lift $ evalStringAndShow env expr
+  outputStrLn out
+
+schemeSettings :: Settings IO
+schemeSettings = defaultSettings {historyFile = Just ".schistory"}
+
+repl' :: (String -> InputT IO ()) -> IO ()
+repl' a = runInputT schemeSettings $ loop a
+  where
+    loop :: (String -> InputT IO ()) -> InputT IO ()
+    loop action = do
+      input <- getInputLine "λ "
+      case input of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just "exit" -> return ()
+        Just (';' : _) -> loop action
+        Just "" -> loop action
+        Just x -> action x >> loop action
+
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [] -> repl
+    [] -> do
+      env <- buildEnvironment
+      repl' (evalAndPrint env)
     ["-h"] -> help
     ["--help"] -> help
     _ -> help
